53a8593d4f216e45f275cbb03db2d0f7965e3d39
Ben Willett <ben.willett@distortenterprises.com>
2020-07-26 17:09:13 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+#adding remote state storage for remote state management on Terraform Cloud#
+terraform {
+ backend "remote" {
+  organization = "Techcamp01"
+
+        workspaces {
+         name = var.workspace
+}
+}
+}
+
 # Azure provider non-beta versions between 1.32.0 and 1.33.0 using the ~> pessimistic constrain operator #
 provider "azurerm" {
   version = "~>1.32.0"
@@ -12,17 +23,16 @@ provider "azurerm" {
 # create a new resource group #
 resource "azurerm_resource_group" "rg1" {
   name = "myTFResourceGroup"
-  location = "westus"
+  location = var.location
   tags = {
-    Environment = "Production"
-    Team = "DevOps"
+    var.tags
 }
 }
  # create a virtual network#
 resource "azurerm_virtual_network" "vnet" {
   name = "myTFVnet"
   address_space = ["10.0.0.0/16","10.1.0.0/16","10.2.0.0/16"]
-  location  = "westus2"
+  location  = var.location
   
   # Terraform versions below 0.12 require interpolation syntax ${...}, from 0.12 and beyond the following exression is used #
   resource_group_name = azurerm_resource_group.rg1.name
@@ -38,7 +48,7 @@ resource "azurerm_subnet" "subnet1" {
 #create a public IP#
 resource "azurerm_public_ip" "publicip1" {
   name  = "myTFPublicIP"
-  location  = "westus2"
+  location  = var.location
   resource_group_name = azurerm_resource_group.rg1.name
   allocation_method = "Static"
   }
@@ -46,7 +56,7 @@ resource "azurerm_public_ip" "publicip1" {
 #Create Network Security Group and rule#
 resource "azurerm_network_security_group" "nsg1" {
   name  = "myTFNSG"
-  location  = "westus2"
+  location  = var.location
   resource_group_name = azurerm_resource_group.rg1.name
   
   security_rule {
@@ -65,7 +75,7 @@ resource "azurerm_network_security_group" "nsg1" {
 #Create network interface#
 resource "azurerm_network_interface" "nic1" {
   name  = "myNIC"
-  location  = "westus2"
+  location  = var.location
   resource_group_name = azurerm_resource_group.rg1.name
   network_security_group_id = azurerm_network_security_group.nsg1.id
   
@@ -80,7 +90,7 @@ resource "azurerm_network_interface" "nic1" {
 #creat a linux virtual machine#
 resource "azurerm_virtual_machine" "vm1" {
   name  = "myTFVM"
-  location  = "westus2"
+  location  = var.location
   resource_group_name = azurerm_resource_group.rg1.name
   network_interfae_ids  = [azurerm_network_interface.nic1.id]
   vm_size = "Standard_DS1_v2"
